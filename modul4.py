9724e88066067e7c4ea7ce6d62583c5f10201062
1Baotianx <eric.botander@elev.ga.dbgy.se>
2025-09-24 14:02:03 +0200

    modul4

---
+++ modul4.py
@@ -0,0 +1,73 @@
+import os 
+os.system ("cls")
+
+#inmatning program med q
+
+
+
+
+# print("Ange q för att avsluta")
+
+# while True:
+
+    
+
+#     inmat = str(input("Skriv in något: "))
+
+#     if inmat == "q":
+#         print("Hejdå")
+#         break
+        
+#     else :
+#         print(inmat)
+
+#Inmatning program 10 gånger
+
+# print("Ange stop för att avsluta")
+
+# inmat2 = str(input("Skriv in något: "))
+
+# for i in range(10): 
+
+#     if inmat2 == "stop":
+    
+#         print("Hejdå")
+#         break
+
+#     else :
+#         print(inmat2)
+
+# Heltal mellan 1,10
+
+# for i in range(1, 11):
+#     print(i)
+
+
+#heltal mellan 1 och användares val
+
+
+# x = int(input("Skriv ett valfritt heltal att räkna till från 1: ") )
+
+
+# for i1 in range(0, x):
+
+#     print(i1+1)
+   
+
+
+#Gångertabell
+
+# for t1 in range(1, 11):
+#     for t2 in range(1, 11):
+#         print(f"{t1} * {t2} = {t2*t1}" 
+
+
+# Överkurs
+# potens = 1
+# bas=int(input("Skriv in basen: "))
+# exponent=int(input("Skriv in exponenten: "))
+
+# for u in range(exponent):
+#     potens *= bas
+
+# print(potens)
